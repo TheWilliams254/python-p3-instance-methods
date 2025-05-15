a9b522ccabb87773b87f77c84494b6a5efb25929
william <williamwambugu663@gmail.com>
2025-05-15 08:23:08 +0300

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,14 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+
+    def talk(self):
+        print("Hello World!")
     pass
+
+    def walk(self):
+        print("The person is walking.")
+    pass
+John = Person()
+John.talk()
+John.walk()
